5b1aedec578c340e4881350bd39c69775318a871
wallanpsantos <wallanpereira09@gmail.com>
2022-02-23 22:57:04 -0300

    Cont. 43. Verificando a existência de um livro pelo ISBN na base de dados

---
+++ src/test/java/com/libraryapi/model/repository/BookRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.libraryapi.model.repository;
+
+import com.libraryapi.repository.BookRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest // Para teste de integração com banco de dados utiliza o H2 em memoria para o teste
+public class BookRepositoryTest {
+
+    @Autowired
+    TestEntityManager testEntityManager;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    @DisplayName("Deve retornar verdadeiro quando existir um livro na base com isbn informado")
+    void returnTrueWhenIsbnExists() {
+        //  Cenario
+        String isbn = "123";
+
+        //  Execução
+        boolean exists = bookRepository.existsByIsbn(isbn);
+
+        //  Verificação
+        assertThat(exists).isTrue();
+    }
+}
