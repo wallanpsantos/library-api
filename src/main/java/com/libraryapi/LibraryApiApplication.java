3f1bcb2bdb3fa78b651cc7777219160f9912c46b
wallanpsantos <wallanpereira09@gmail.com>
2022-05-29 01:53:27 -0300

    86. Testando o envio de emails

--- src/main/java/com/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/libraryapi/LibraryApiApplication.java
@@ -1,12 +1,16 @@
 package com.libraryapi;
 
+import com.libraryapi.service.EmailService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,17 +18,21 @@ public class LibraryApiApplication {
 
     @Value("${loan.overdue.message}")
     private String message;
-
+    @Autowired
+    private EmailService emailService;
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
-    @Scheduled(cron = "0/20 * * 1/1 * ?")
-    public void startRoutineTest() {
-        System.out.println("Hi, i'am routine to test :D");
-        System.out.println(message);
+    @Bean
+    public CommandLineRunner runner() {
+        return args -> {
+            var emails = List.of("wallanpereira09@gmail.com");
+            emailService.sendEmail(message, emails);
+            System.out.println("Executado envio de e-mail :D");
+        };
     }
 
     public static void main(String[] args) {
